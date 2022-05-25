d9373c7891a00ca5da21a00380448b032df07819
Alex Nikolov <no@spam.com>
2022-05-25 20:20:10 +1000

    BC Working

--- BackwardChaining.java
+++ BackwardChaining.java
@@ -4,113 +4,102 @@ import java.util.LinkedList;
 
 public class BackwardChaining extends SolveMethod
 {
-    //A table of symbols, where the count is the number of symbols in each clause's premise.
-    HashMap<String, Integer> count = new HashMap<String, Integer>();
-    
-    //This is the queue of symbols to be evaluated.
-    LinkedList<String> queue = new LinkedList<String>();
-    
-    //This is the table of symbols that lists which symbols are given as fact.
-    HashMap<String, Boolean> given = new HashMap<String, Boolean>();
-
-    //Using a linked list of strings to add symbols onto as they are proven true to maintain the order in which they were proven.
-    LinkedList<String> provenInOppositeOrder = new LinkedList<String>();
-    
     BackwardChaining()
     {
         name = "BC";
     }
 
+    //This is the table of symbols that lists which symbols are given as fact.
+    HashMap<String, Boolean> given = new HashMap<String, Boolean>();
 
+    //Global variable used to store the successful path.
+    LinkedList<String> solution = new LinkedList<String>();
+
+    
     public String Solve(Problem inputProblem)
     {
         //Only the symbols given as facts are true at the start.
         InitialiseGiven(inputProblem);
-
-        //The count for each statement is initially the number of symbols in each clause's premise
-        InitialiseCount(inputProblem);
-
-        //puts all symbols initially known to be true in the queue.
-        InitialiseQueue(inputProblem);
-
-         //checks if the query has already been given as a fact.
-         if (given.get(inputProblem.query))
-         {
-             String solution = "Yes: ";
-             for(String s : provenInOppositeOrder)
-             {
-                 solution += s + ", ";
-             }
-             solution += inputProblem.query;
-             return solution;
-         }
-
-
-        while(!queue.isEmpty())
+        
+        LinkedList<String> startingPath = new LinkedList<String>();
+        startingPath.add(inputProblem.query);
+        
+        if(BC_AND_OR(inputProblem.knowledgeBase.sentences, inputProblem.query, startingPath) != null)
         {
-            String p = PopQueue();
-
-           
-            ArrayList<ArrayList<String>> premises = GetPremises(p, inputProblem);
-
-            for(ArrayList<String> premise: premises)
+            String returnLine = "Yes: ";
+            for(int i = 0; i < solution.size()-1; i++)
             {
-                Boolean premisesTrue = true;
-                for(String premiseSymbol: premise)
-                {
-                    Boolean dupe = false;
-                    for(String s : queue)
-                    {
-                        if(s.compareTo(premiseSymbol) == 0)
-                        {
-                            dupe = true;
-                        }
-                    }
-                    if(!dupe)
-                    {
-                        queue.add(premiseSymbol);
-                        provenInOppositeOrder.addFirst(premiseSymbol);
-                    }
-                    if(!given.get(premiseSymbol) || (count.get(premiseSymbol) == -1))
-                    {
-                        premisesTrue = false;
-                    }
-                }
-                if(premisesTrue)
-                {
-                    String solution = "Yes: ";
-                    for(String s : provenInOppositeOrder)
-                    {
-                        solution += s + ", ";
-                    }
-                    solution += inputProblem.query;
-                    return solution;
-                }
+                returnLine += solution.get(i) + ", ";
             }
-/*
-            //checks if the solution has been found
-            if (count.get(p) == 0)
-            {
-                String solution = "Yes: ";
-                for(String s : provenInOrder)
-                {
-                    solution += s + ", ";
-                }
-                solution += p;
-                return solution;
-            }*/
+            returnLine += solution.getLast();
+            return returnLine;
         }
-
-        String solution = "No";
-        return solution;
+        else
+        {
+            return "No.";
+        }
     }
 
+
+    private LinkedList<String> BC_AND_OR(ArrayList<String> KB, String query, LinkedList<String> path)
+    {
+        String currentSymbol = path.getFirst();
+        solution.addFirst(currentSymbol);
+        
+        //checks if the solution has been found
+        if (given.get(currentSymbol))
+        {
+            return path;
+        }
+        
+        ArrayList<ArrayList<String>> premises = GetPremises(currentSymbol, KB);
+
+        if(premises.isEmpty())
+        {
+            solution.clear();
+            solution.addLast(path.getLast());
+            return null;
+        }
+
+
+        for(ArrayList<String> premise: premises)
+        {
+            Boolean allTrue = true;
+            for(String premiseSymbol: premise)
+            {
+                LinkedList<String> newpath = new LinkedList<String>();
+                for(String s : path)
+                {
+                    newpath.add(s);
+                }
+                newpath.addFirst(premiseSymbol);
+                
+                if(BC_AND_OR(KB, premiseSymbol, newpath) == null)
+                {
+                    allTrue = false;
+                }
+            }
+            if(allTrue)
+            {
+                for(String premiseSymbol: premise)
+                {
+                    path.addFirst(premiseSymbol);
+                }
+
+                return path;
+            }
+
+        }
+
+        return null;
+    }
+    
     //returns the premises/bodies that must be true in order for a symbol to also be true.
-    private ArrayList<ArrayList<String>> GetPremises(String head, Problem prob)
+    private ArrayList<ArrayList<String>> GetPremises(String head, ArrayList<String> KB)
     {
         ArrayList<ArrayList<String>> results = new ArrayList<ArrayList<String>>();
 
-        for (String sentence : prob.knowledgeBase.sentences)
+        for (String sentence : KB)
         {            
             String[] hornBodyHead =  sentence.split("(=>)");
             if(hornBodyHead.length > 1)
@@ -131,28 +120,6 @@ public class BackwardChaining extends SolveMethod
         return results;
     }
 
-
-    //counts the number of unproven premises for each symbol. This is zero if it is a given fact, and -1 if it is neither implied, nor a given fact.
-    private void InitialiseCount(Problem prob)
-    {
-        for (String sym : prob.knowledgeBase.symbols)
-        {
-            ArrayList<ArrayList<String>> premises = GetPremises(sym, prob);
-            int num = premises.size();            
-            if((premises.size() == 0) && (!given.get(sym)))
-            {
-                num = -1;
-            }
-            this.count.put(sym, num);
-        }
-    }
-
-    //puts all symbols initially known to be true in the queue.
-    private void InitialiseQueue(Problem prob)
-    {
-        queue.add(prob.query);
-    }
-
     //All symbols are initially not inferred.
     private void InitialiseGiven(Problem prob)
     {
@@ -170,13 +137,5 @@ public class BackwardChaining extends SolveMethod
                 given.replace(sentence, true);
             }
         }
-    }
-
-    //pops the first symbol off the queue.
-    private String PopQueue()
-    {
-        String popped = queue.getLast();
-        queue.removeLast();
-        return popped;
     }
 }
